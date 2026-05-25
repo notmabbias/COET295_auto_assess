043b291493a7764c2db8043f8710a21d366c75e5
notmabbias <sheehan1821@saskpolytech.ca>
2026-05-25 14:05:48 -0600

    began implementation of openai api and testing

--- app.py
+++ app.py
@@ -6,3 +6,28 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('hello.html')
+
+
+@app.route('/search')
+def search_page():
+    return render_template('search.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze_search():
+        # use request to store form contents
+        car_make = request.form.get('make')
+        car_model = request.form.get('model')
+
+        car_listing = request.form.get('listing_text')
+        car_carfax = request.form.get('carfax_text')
+
+        # cast to int since form sends strings
+        car_year = int(request.form.get('year'))
+
+        print(f"user searched for: {car_year} {car_make} {car_model}\nListing Text:\n{car_listing}\nCarfax Text\n{car_carfax}")
+
+        return "<p>searching...</p>"
+
+@app.route('/deb')
+def video_page():
+    return render_template('video_page.html')
